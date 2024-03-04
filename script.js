ac1013ee8d61f5d254a1c3776922dc45fc75ddd5
Nick Sanchez <nicksanchez2371@gmail.com>
2024-03-03 21:41:36 -0600

    found the issue

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
 
+console.log('hey')
+let myLibrary = [];
 
 
+function book() {
 
 
+}
+function addBookToLibrary() {
+
+}
 
 
-
-
-
+let newBookBtn = document.querySelector("#new-book-btn");
+newBookBtn.addEventListener("click", function() {
+  alert('hello')
+  // let newBookForm = document.querySelector('#new-book-form');
+  // newBookForm.computedStyleMap.display = 'block';
+})
 
 
 
