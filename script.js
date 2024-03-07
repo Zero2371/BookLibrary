8ca27f50549f58c947f6a106cff174792dfe37c5
Nick Sanchez <nicksanchez2371@gmail.com>
2024-03-06 22:56:51 -0600

    added a book card that shows the title, author, pages and if it has been read

--- script.js
+++ script.js
@@ -11,25 +11,49 @@ this.author = author;
 this.pages = pages;
 this.read = read;
 }
+Book.prototype.toggleRead = function() {
+  this.read = !this.read
+}
 
-function render(){
+function toggleRead(index) {
+  myLibrary[index].toggleRead;
+  render();
+}
+
+
+ function render(){
   let libraryEl= document.querySelector('#library');
   libraryEl.innerHTML = '';
   for(let i = 0; i < myLibrary.length; i++) {
-    let book = myLibrary[1];
+    let book = myLibrary[i];
     let bookEl = document.createElement("div");
-    bookEl.innerHTML = `<p>${book.title}</p>`
+    bookEl.setAttribute('class', 'book-card');
+    bookEl.innerHTML = `
+    <div class='card-header'>
+    <h3 class='title'>${book.title}</h3>
+    <h4 class='author'>by ${book.author}</h4>
+    </div>
+    <div class='book-body'>
+    <p>${book.pages} pages</p>
+    <p class="read-status>${book.read ? "Read" : "Not read yet"}</p> 
+    <button class='remove-btn' onclick='removeBook(${i}'>Remove</button>
+    </div>`;
     libraryEl.appendChild(bookEl); 
   }
 }
+
+function removeBook(index) {
+  myLibrary.splice(index, 1);
+  render();
+}
 function addBookToLibrary() {
 let title = document.querySelector('#title').value;
-let author = document.querySelector('#author').value;
-let pages = document.querySelector('#pages').value;
-let read = document.querySelector('#read').checked;
+let author = document.getElementById('author').value;
+let pages = document.getElementById('pages').value;
+let read = document.getElementById('read').checked;
 let newBook = new Book(title, author, pages, read);
 myLibrary.push(newBook);
-render()
+render();
 }
 
 
@@ -41,7 +65,7 @@ newBookBtn.addEventListener("click", function() {
 
 document.querySelector('#new-book-form').addEventListener("submit", function() {
   event.preventDefault();
-  addBookToLibrary()
+  addBookToLibrary();
 })
 
 
